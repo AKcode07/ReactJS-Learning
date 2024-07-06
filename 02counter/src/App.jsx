6145ae11085261f93be5b970af8ddba4e5303f5f
AKcode07 <ashwinkumavat07@gmail.com>
2024-07-06 14:32:42 +0530

    6th July 2024

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let counter = 10;
+  const [counter, setCounter] = useState(10);
+  // let counter = 10;
   const addValue = () => {
-    counter += 1;
-    console.log(counter);
+    setCounter(counter + 1);
+
+    // counter += 1;
+    // console.log(counter);
+  };
+
+  const remove = () => {
+    setCounter(counter - 1);
+
+    // counter += 1;
+    // console.log(counter);
   };
 
   return (
@@ -16,7 +24,7 @@ function App() {
       <h2>Counter value: {counter} </h2>
       <button
       onClick={addValue}>Add value</button>
-      <button>Remove value</button>
+      <button onClick={remove}>Remove value</button>
       <p>footer: {counter} </p>
     </>
   )
