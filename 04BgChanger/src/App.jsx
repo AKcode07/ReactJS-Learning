892be6532344dabce829473ee00da9997f4c5fc3
AKcode07 <ashwinkumavat07@gmail.com>
2024-07-09 17:29:51 +0530

    9th July 2024

--- 04BgChanger/src/App.jsx
+++ 04BgChanger/src/App.jsx
@@ -4,12 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('orange')
 
   return (
-    <>
-      <h1 className='bg-orange-600 text-3xl'>Bg Changer App with Vite</h1>
-    </>
+     <div className='w-full h-screen duration-200 ' style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button
+          onClick={() => setColor('red')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor: 'red'}}
+          >Red</button>
+          <button
+          onClick={() => setColor('green')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor: 'green'}}
+          >Green</button>
+          
+          </div>
+      </div>
+    </div>
   )
 }
 
