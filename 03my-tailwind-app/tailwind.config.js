1eec35a30e7eaa56b60b4bb970e1e8c67a06145c
AKcode07 <ashwinkumavat07@gmail.com>
2024-07-09 17:01:14 +0530

    9th July 2024

--- 03my-tailwind-app/tailwind.config.js
+++ 03my-tailwind-app/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
 
