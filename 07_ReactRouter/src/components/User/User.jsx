6f62e5891dec0b4c26a9da14b8dac33fb6421a98
AKcode07 <ashwinkumavat07@gmail.com>
2024-07-12 17:28:09 +0530

    12th July 2024

--- 07_ReactRouter/src/components/User/User.jsx
+++ 07_ReactRouter/src/components/User/User.jsx
@@ -4,7 +4,12 @@ import {useParams} from "react-router-dom"
 function User() {
   const {userid} = useParams()
   return (
-    <div className='bg-orange-500 text-black text-3xl text-center py-5'>User: {userid}</div>
+    <div className='bg-orange-500 text-black text-3xl text-center py-5'>
+      User: {userid}
+      <p className='bg-slate-400 text-2xl'>Write UserId in URL</p>
+    </div>
+    
+    
   )
 }
 
--- 07_ReactRouter/src/main.jsx
+++ 07_ReactRouter/src/main.jsx
@@ -8,12 +8,14 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import User from './components/User/User.jsx'
 import Github, {githubInfoLoader} from './components/Github/Github.jsx'
+import Contact from './components/Contact/Contact.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />}/>
       <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
       <Route path='user/' element={<User />} >
         <Route path=':userid' element={<User />} />
       </Route>
