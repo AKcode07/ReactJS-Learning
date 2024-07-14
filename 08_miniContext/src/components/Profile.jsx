a215d123408e1022cf65072e3f1373ff8e9fd231
AKcode07 <ashwinkumavat07@gmail.com>
2024-07-14 23:39:31 +0530

    14th July 2024

--- 08_miniContext/src/components/Profile.jsx
+++ 08_miniContext/src/components/Profile.jsx
@@ -3,7 +3,6 @@ import UserContext from '../context/UserContext'
 
 function Profile() {
   const {user} = useContext(UserContext)
-
   if(!user) return <h1>Not logged in </h1>
   return (
     <div>
